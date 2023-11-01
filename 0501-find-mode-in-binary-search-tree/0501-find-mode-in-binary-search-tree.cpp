2d2e60aad8c8ec176d82a8f818d36f6483c3d972
Amit Kumar <79916998+kumaramit13@users.noreply.github.com>
2023-11-01 13:38:33 +0530

    Time: 15 ms (69.51%), Space: 26.4 MB (9.36%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    // Do, Inorder traversal and store in a vector, all nodes
+    // use map, to count frequency of each node value using the vector
+    // find out the max freq from the map
+    // iterate over the map, and check which node values are havig frequency == mxfreq
+    // store those in the result array and return
+    
+    vector<int>v;
+    
+    void in(TreeNode* root) {
+        if(!root) return;
+        in(root->left);
+        v.push_back(root->val);
+        in(root->right);
+    }
+    
+    vector<int> findMode(TreeNode* root) {
+        in(root); // call the inorder
+        
+        map<int, int> mp;
+        
+        vector<int> ans;  // to store and return our answer
+        for(int x : v) mp[x]++; // counting frequency of each node values
+        
+        int mxfreq = -1;
+        for(auto it : mp) {
+            mxfreq = max(it.second, mxfreq); // looking for max frequency
+        }
+        for(auto it : mp) {
+            if(it.second == mxfreq) { // check which is having frequency == mxfreq
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
